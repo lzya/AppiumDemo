5ad5d4f9193e3ba7e1e42e422399842c6065f04a
lizhuoya <lizhuoya@dxy.cn>
2021-02-03 20:56:19 +0800

    del

--- AppiumDemo08_Android/Search.py
+++
@@ -1,9 +0,0 @@
-
-class Search(object):
-
-    def search(self, keyword):
-        pass
-        # self.drive.find("").click()
-
-    def getStocks(self):
-        return "拼多多"
--- AppiumDemo08_Android/testcase/test_ut.py
+++
@@ -1,3 +0,0 @@
-def test_xpth():
-    import xml.etree.ElementTree as ET
-    root = ET.fromstring('')
